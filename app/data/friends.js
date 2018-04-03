eefa6f3956a875649496e52ebe644db333e73f2e
Klara Boyer <kawboyer@gmail.com>
2018-04-03 19:38:02 -0400

    added for loop and started comparing arrays

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,98 +4,32 @@ var surveyArray = [
   {
     "name": "Grace Hopper",
     "photo": "https://www.biography.com/.image/t_share/MTE5NTU2MzE2NjYxNTE1Nzg3/grace-hopper-21406809-1-402.jpg",
-    "scores": [
-      3,
-      3,
-      3,
-      2,
-      4,
-      2,
-      2,
-      1,
-      4,
-      3
-    ]
+    "scores": [ 3, 3, 3, 2, 4, 2, 2, 1, 4, 3]
   },
   {
     "name": "Annie Easley",
     "photo": "http://www.nasa.gov/sites/default/files/thumbnails/image/easley.jpg",
-    "scores": [
-      2,
-      2,
-      1,
-      1,
-      4,
-      3,
-      5,
-      5,
-      2,
-      1
-    ]
+    "scores": [2, 2, 1, 1, 4, 3, 5, 5, 2, 1]
   },
   {
     "name": "Adele Goldberg",
     "photo": "https://www.computerhope.com/people/pictures/adele_goldberg.jpg",
-    "scores": [
-      2,
-      5,
-      5,
-      3,
-      1,
-      1,
-      3,
-      4,
-      5,
-      1
-    ]
+    "scores": [2, 5, 5, 3, 1, 1, 3, 4, 5, 1]
   },
   {
     "name": "Karen Spärck Jones",
     "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Karen_Sp%C3%A4rck.jpg/220px-Karen_Sp%C3%A4rck.jpg",
-    "scores": [
-      1,
-      2,
-      5,
-      5,
-      4,
-      3,
-      1,
-      2,
-      2,
-      5
-    ]
+    "scores": [1, 2, 5, 5, 4, 3, 1, 2, 2, 5]
   },
   {
     "name": "Donna Dubinsky",
     "photo": "https://www.alumni.hbs.edu/PublishingImages/stories/thumbs/Dubinsky_Donna_527x333.jpg",
-    "scores": [
-      5,
-      5,
-      5,
-      4,
-      5,
-      5,
-      5,
-      4,
-      5,
-      1
-    ]
+    "scores": [5, 5, 5, 4, 5, 5, 5, 4, 5, 1]
   },
   {
     "name": "Angelica Ross",
     "photo": "https://www.advocate.com/sites/advocate.com/files/2015/08/31/Angelica-Ross-400x300.jpg",
-    "scores": [
-      4,
-      1,
-      2,
-      1,
-      2,
-      1,
-      5,
-      5,
-      1,
-      2
-    ]
+    "scores": [4, 1, 2, 1, 2, 1, 5, 5, 1, 2]
   }
 ];
 
--- server.js
+++ server.js
@@ -18,23 +18,41 @@ app.use(bodyParser.json());
 require("./app/routes/apiRoutes")(app);
 require("./app/routes/htmlRoutes")(app);
 
+// Convert the input data into an array of numbers
+
+// Calculate the sum of the input data
+
+// Compare the difference between current user data and saved data.
+var isEqual = function (value, other) {
+// 
+  var valueLength = surveyArray;
+  var otherLength = inputArray;
+  
+  if (valueLength !== otherLength) {
+    return false
+  }
+
+  return true;
+};
+ 
+var surveyArray = [];
+var inputArray = [];
+
+for (var i = 0; i < surveyArray.length; i++) {
+  for(var j = 0; i < (inputArray.scores).length; i++) {
+    if (surveyArray[i] === inputArray[i]) {
+      // COMPARE SUMS
+    }
+  }
+}
+
+// Calculate the "totalDifference" (use absolute value)
+
+// Determine the match
+
+// Modal pop-up with name and picture
+
 // LISTENER - start the server
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
-
-
-// OBS!!!!!!!
-// 6. Determine the user's most compatible friend using the following as a guide:
-
-//    * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
-//    * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
-//      * Example: 
-//        * User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
-//        * User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
-//        * Total Difference: **2 + 1 + 2 =** **_5_**
-//    * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on. 
-//    * The closest match will be the user with the least amount of difference.
-
-// 7. Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-//    * The modal should display both the name and picture of the closest match. 
